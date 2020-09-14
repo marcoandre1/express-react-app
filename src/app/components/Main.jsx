369d7026d975e6fb9f81161d23205ad453f7c777
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-09-14 00:25:01 -0400

    remove history file

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
-import { ConnectedDashboard } from "./Dashboard";
-import { Router, Route } from 'react-router-dom';
-import { history } from "../store/history";
-import { ConnectedNavigation } from "./Navigation";
+import { ConnectedDashboard } from './Dashboard';
+import { BrowserRouter, Route, } from 'react-router-dom';
+import { ConnectedNavigation } from './Navigation';
 
 export const Main = ()=>(
-    <Router history={ history }>
+    <BrowserRouter>
         <Provider store={store}>
             <div className="container mt-3">
                 <ConnectedNavigation/>
                 {/*<ConnectedDashboard/>*/}
                 <Route
                     exact
-                    path={"/dashboard"}
+                    path="/dashboard"
                     render={ () => (<ConnectedDashboard/>)}
                 />
             </div>
         </Provider>
-    </Router>
+    </BrowserRouter>
 );
